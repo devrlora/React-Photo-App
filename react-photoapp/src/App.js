e18bb90e29e7612412c57d9d117d5b458849f00d
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 10:40:10 -0400

    import navbar first to set up route links

--- react-photoapp/src/App.js
+++ react-photoapp/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Navbar'
+import Navbar from './components/Navbar'
+import {Route} from 'react-router-dom'
+
+
 function App() {
   return (
     <div className="App">
