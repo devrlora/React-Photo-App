0d9f462bbe095b5a1b3a361552fed3670f9853c7
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 19:46:33 -0400

    comment out navbar component from file

--- react-photoapp/src/App.js
+++ react-photoapp/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Login />
-      
+      {/* <Navbar /> */}
     </div>
   );
 }
