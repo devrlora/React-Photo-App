059bd399cae63e0bf6c56d4602573824485e0d14
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 11:47:55 -0400

    add login component

--- react-photoapp/src/components/Navbar.js
+++ react-photoapp/src/components/Navbar.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
 import Home from './Home';
 import User from './User';
-// import Bookmarks from './Bookmarks';
-// import Logout from './Logout';
+import Bookmarks from './Bookmarks';
+import Logout from './Logout';
 
 
 export class Navbar extends Component {
     render() {
         return (
             <div className ='Menu'>
+                {/* <Route path ='/login' component={Login} /> */}
                 <Route path ='/home' component={Home} />
                 <Route path ='/user' component={User} />
-                {/* <Route path ='/bookmarks' component={Bookmarks} />
-                <Route path ='/' component={Logout} /> */}
+                <Route path ='/bookmarks' component={Bookmarks} />
+                <Route path ='/logout' component={Logout} />
             </div>
         )
     }
