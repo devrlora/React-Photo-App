c7557f30e25e2aef5796754a37162a6302add592
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 11:47:08 -0400

    add test text

--- react-photoapp/src/components/Logout.js
+++ react-photoapp/src/components/Logout.js
@@ -2,3 +2,17 @@
 // Boolean? If clicked = true =>
 // Log current user out
 // Link back to login page
+
+import React, { Component } from 'react'
+
+export class Logout extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the logout function page. This will link to login page</h1>
+            </div>
+        )
+    }
+}
+
+export default Logout
