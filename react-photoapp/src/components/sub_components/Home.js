b8459574cd67f209c3bf32c257f48826aaf0ba4a
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 10:52:03 -0400

    add test div

--- react-photoapp/src/components/sub_components/Home.js
+++ react-photoapp/src/components/sub_components/Home.js
@@ -1,1 +1,14 @@
 // Will Link to a Random Photo-Grid
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
