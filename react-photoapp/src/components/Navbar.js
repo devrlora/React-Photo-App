8a7a49316865600ceb2cbe3d32efbf7f350814ba
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 12:03:28 -0400

    add exact paths and links for path navigation

--- react-photoapp/src/components/Navbar.js
+++ react-photoapp/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import Home from './Home';
 import User from './User';
 import Bookmarks from './Bookmarks';
@@ -10,11 +10,19 @@ export class Navbar extends Component {
     render() {
         return (
             <div className ='Menu'>
+                <nav className = 'Nav-bar'>
+                    
+                    <Link to = '/home'>Home</Link>
+                    <Link to = '/user'>User</Link>
+                    <Link to = '/bookmarks'>Bookmarks</Link>
+                    <Link to = '/logout'>Logout</Link>
+                    {/* use 'exact' to prevent multiple pages loading */}
                 {/* <Route path ='/login' component={Login} /> */}
-                <Route path ='/home' component={Home} />
-                <Route path ='/user' component={User} />
-                <Route path ='/bookmarks' component={Bookmarks} />
-                <Route path ='/logout' component={Logout} />
+                <Route exact path ='/home' component={Home} />
+                <Route exact path ='/user' component={User} />
+                <Route exact path ='/bookmarks' component={Bookmarks} />
+                <Route exact path ='/' component={Logout} />
+                </nav>
             </div>
         )
     }
