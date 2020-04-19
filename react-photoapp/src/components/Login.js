e50cd460f478625311e6cc6323a52cefdd824909
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 15:32:42 -0400

    define login component

--- react-photoapp/src/components/Login.js
+++ react-photoapp/src/components/Login.js
@@ -1,10 +1,29 @@
 // Login path will lead to one of three user accounts
 import React, { Component } from 'react'
+import Navbar from './Navbar';
+
 
 export class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            userName: '',
+            passWord: ''
+        }
+        this.handleChange=this.handleChange.bind(this);
+    }
+    handleChange(e){
+this.setState({userName: evt.target.value});
+    }
     render() {
         return (
             <div>
+                <form>
+                <input type='text' 
+                    value= {this.state.userName} 
+                    onChange={this.handleChange}
+                />
+                </form>
                 
             </div>
         )
