e489e93f68ec035ce81caa62e6330b52e6ed5384
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 19:36:46 -0400

    removed routing from file, restored navbar and login component elements in return div

--- react-photoapp/src/App.js
+++ react-photoapp/src/App.js
@@ -1,23 +1,15 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
-
-class App extends Component{
-  render(){
-    return (
-      <div className="App">
-        <BrowserRouter>
-        {/* Will route user to login page, then after authenticating,to the navbar */}
-        <Switch>
-            <Route exact path ={'/'} render={...props} loggedIn={this.state.loggedIn}/>
-            <Route exact path ={'/navbar'} component={Navbar} />
-          </Switch>
-        </BrowserRouter>
-  </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Login />
+      
+    </div>
+  );
 }
 
 export default App;
