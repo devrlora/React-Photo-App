49fe4a49276c9351f9db08a1903c2a6d17841823
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-18 20:25:42 -0400

    import navbar to file

--- react-photoapp/src/App.js
+++ react-photoapp/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from './Navbar'
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
