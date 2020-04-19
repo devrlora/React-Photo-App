cd79b59db65311509445aa68b8a53ad7faba1c11
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 15:59:59 -0400

    add handlesubmit function to handle use input info

--- react-photoapp/src/components/Login.js
+++ react-photoapp/src/components/Login.js
@@ -13,23 +13,38 @@ class Login extends Component {
             passWord: ''
         }
         this.handleChange=this.handleChange.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
     }
     // function will target any input the user types in real time 
     // and render the page with each keystroke.
-
     handleChange(evt){
 this.setState({userName: evt.target.value});
+    }
+    // function will pass the input info 
+    handleSubmit(evt){
+        // prevent page refreshing 
+        evt.preventDefault();
+        alert(`You typed: ${this.state.userName}` )
+        this.setState({userName: ''});
     }
     render() {
         return (
             <div>
                 <h1>Login Form</h1>
-                <form>
+                <form onSubmit= {this.handleSubmit}>
                 <input className ='login-box'
                     type='text' 
-                    value= {this.state.userName} 
+                    value= {this.state.userName}
+                    placeholder='Username' 
                     onChange={this.handleChange}
                 /> 
+                <input className ='login-box'
+                    type='text' 
+                    value= {this.state.userName}
+                    placeholder='Password' 
+                    onChange={this.handleChange}
+                /> 
+                <button className ='login-btn' onClick={this.handleSubmit}>Enter</button>
                 </form>
                 
             </div>
