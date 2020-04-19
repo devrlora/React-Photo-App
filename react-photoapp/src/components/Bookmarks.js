f4c1a6016ebc5b45be9b0e319d25ecbccaf00db4
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 11:46:19 -0400

    add test text

---
+++ react-photoapp/src/components/Bookmarks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Bookmarks extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This will show saved pics</h1>
+            </div>
+        )
+    }
+}
+
+export default Bookmarks
