ebb773cee0ed68be5ec8053856b41c3afa83317d
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 20:19:14 -0400

    add a function that renders a random picture

--- react-photoapp/src/components/Home.js
+++ react-photoapp/src/components/Home.js
@@ -1,11 +1,17 @@
 // Will Link to a Random Photo-Grid
 import React, { Component } from 'react'
+const UnsplashAPI = 'https://picsum.photos/200';
+
 
 export class Home extends Component {
     render() {
+        let imgSrc = `${UnsplashAPI}`;
         return (
             <div>
                 <h1>Welcome to the Home Page</h1>
+                <div className="Photo-image">
+					<img src={imgSrc} alt= 'random-pic'/>
+				</div>
             </div>
         )
     }
