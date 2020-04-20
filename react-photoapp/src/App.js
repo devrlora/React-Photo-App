2fc0dec6dfd5ea694bec03e7091deef9bb56404d
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 22:57:54 -0400

    add pseudocode

--- react-photoapp/src/App.js
+++ react-photoapp/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 // import Login from './components/Login';
+
+
 function App() {
   return (
     <div className="App">
+      {/* Could not get login component to redirect to navbar */}
       {/* <Login /> */}
       <Navbar />
     </div>
