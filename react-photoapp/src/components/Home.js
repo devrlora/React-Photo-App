41afc3371a2c3f79f3e377690bfd105609f3dcaf
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 20:25:13 -0400

    link photo api to file

--- react-photoapp/src/components/Home.js
+++ react-photoapp/src/components/Home.js
@@ -1,6 +1,6 @@
 // Will Link to a Random Photo-Grid
 import React, { Component } from 'react'
-const UnsplashAPI = 'https://picsum.photos/200';
+const UnsplashAPI = 'https://api.unsplash.com/';
 
 
 export class Home extends Component {
@@ -10,6 +10,11 @@ export class Home extends Component {
             <div>
                 <h1>Welcome to the Home Page</h1>
                 <div className="Photo-image">
+					<img src={imgSrc} alt= 'random-pic'/>
+					<img src={imgSrc} alt= 'random-pic'/>
+					<img src={imgSrc} alt= 'random-pic'/>
+					<img src={imgSrc} alt= 'random-pic'/>
+					<img src={imgSrc} alt= 'random-pic'/>
 					<img src={imgSrc} alt= 'random-pic'/>
 				</div>
             </div>
