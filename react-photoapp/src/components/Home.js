8029574abcdb24c0e4436980eea4aee74c19eeef
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 22:59:15 -0400

    add pseudocode

--- react-photoapp/src/components/Home.js
+++ react-photoapp/src/components/Home.js
@@ -2,11 +2,15 @@
 import React, { Component } from 'react'
 import Photos from '../components/Photos'
 import '../components/Home.css';
+
+
 export class Home extends Component {
     render() {
         return (
+
             <div className='Photo-container'>
                 <h1>Welcome to the Home Page</h1>
+                {/* Photos are hard coded, should be selected from an array*/}
                 <div className="Photo-image">
 					<Photos />
 					<Photos />
@@ -21,7 +25,6 @@ export class Home extends Component {
 					<Photos />
 					<Photos />
                 </div>
-                
             </div>
         )
     }
