00cc75b56476f27ee83b5e8490ad2805683392b9
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-19 22:41:39 -0400

    add hard coded photo components

--- react-photoapp/src/components/Home.js
+++ react-photoapp/src/components/Home.js
@@ -1,22 +1,27 @@
 // Will Link to a Random Photo-Grid
 import React, { Component } from 'react'
-const UnsplashAPI = 'https://api.unsplash.com/';
-
-
+import Photos from '../components/Photos'
+import '../components/Home.css';
 export class Home extends Component {
     render() {
-        let imgSrc = `${UnsplashAPI}`;
         return (
-            <div>
+            <div className='Photo-container'>
                 <h1>Welcome to the Home Page</h1>
                 <div className="Photo-image">
-					<img src={imgSrc} alt= 'random-pic'/>
-					<img src={imgSrc} alt= 'random-pic'/>
-					<img src={imgSrc} alt= 'random-pic'/>
-					<img src={imgSrc} alt= 'random-pic'/>
-					<img src={imgSrc} alt= 'random-pic'/>
-					<img src={imgSrc} alt= 'random-pic'/>
-				</div>
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+                </div>
+                
             </div>
         )
     }
