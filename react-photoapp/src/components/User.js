b4d854344113c4630e86b0b293b70c837eb289e8
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-20 20:37:53 -0400

    added code from home.js file to user.js file

--- react-photoapp/src/components/User.js
+++ react-photoapp/src/components/User.js
@@ -1,14 +1,36 @@
 // use if/else function to verify user login
+// Will Link to a Random Photo-Grid
 import React, { Component } from 'react'
+import Photos from '../components/Photos'
+import '../components/User.css';
+
 
 export class User extends Component {
     render() {
         return (
-            <div>
+
+            <div className='Photo-container'>
                 <h1>Welcome to the User Page</h1>
+                {/* Photos are hard coded, should be selected from an array*/}
+                <div className="Photo-image">
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+					<Photos />
+                </div>
             </div>
         )
     }
 }
 
-export default User
+export default User;
